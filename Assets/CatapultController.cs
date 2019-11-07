8234b6b030c4c0ae6dabe435582c0188d0d1fefa
Romain <romain.ducros@cimpa.com>
2019-11-07 17:03:38 +0100

    Remove to ARcore

---
+++ Assets/CatapultController.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CatapultController : MonoBehaviour
+{
+    //private Vector3 position;
+    private float width;
+    private float height;
+    private float pullStrengh;
+    private Animator _catapultAnimator;
+    private Vector2 _firstTouchPosition;
+
+    //[Range(0f,1f)]
+    //[SerializeField]
+    //private float strenghThreshold = .2f;
+
+    void OnGUI()
+    {
+        // Compute a fontSize based on the size of the screen width.
+        GUI.skin.label.fontSize = (int)(Screen.width / 25.0f);
+
+        GUI.Label(new Rect(20, 20, width, height * 0.25f),
+            "str = " + pullStrengh.ToString("f2"));
+    }
+
+    void Awake()
+    {
+        width = (float)Screen.width / 2.0f;
+        height = (float)Screen.height / 2.0f;
+        _catapultAnimator = GetComponent<Animator>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Handle screen touches.
+        if (Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0);
+
+
+            if (touch.phase == TouchPhase.Began)
+            {
+                Debug.Log("Touch Detected");
+                _catapultAnimator.SetBool("isTouching", true);
+                pullStrengh = 0f;
+                _catapultAnimator.SetFloat("PullStrengh", pullStrengh);
+
+                _firstTouchPosition = touch.position;
+            }
+
+            if (touch.phase == TouchPhase.Moved)
+            {
+                Debug.Log("Touch Moving");
+                Vector2 pos = touch.position;
+                pullStrengh = (_firstTouchPosition.y - pos.y) / height;
+                _catapultAnimator.SetFloat("PullStrengh", pullStrengh);
+            }
+
+            if (touch.phase == TouchPhase.Ended)
+            {
+                Debug.Log("Touch End");
+
+                _catapultAnimator.SetBool("isTouching", false);
+            }
+
+        }
+    }
+
+    public void ThrowRock()
+    {
+        Debug.Log("ThrowRock");
+    }
+
+}
---
+++ Assets/_Project/Scripts/ControllerPlane.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
+using UnityEngine.UI;
+
+public class ControllerPlane : MonoBehaviour
+{
+    //INSTANTIATE AT START OR IN EDITOR
+    ARRaycastManager raycastManager;
+
+    //DEBBUG / TEST
+    public Text TextDebugARRaycast;
+
+
+    public GameObject LevelPrefab;
+    public GameObject Catapult;
+    public GameObject levelWrapper;
+
+    private bool isLevelCreated = false; 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        raycastManager = GetComponent<ARRaycastManager>();
+        Catapult.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.touchCount == 1)
+        {
+            List<ARRaycastHit> hitResults = new List<ARRaycastHit>();
+            raycastManager.Raycast(Input.GetTouch(0).position,hitResults, TrackableType.Planes);
+
+            if (hitResults.Count > 0)
+            {
+                foreach (ARRaycastHit hit in hitResults)
+                {
+                    Debug.Log(hit.trackableId);
+                    TextDebugARRaycast.text = hit.pose.position.ToString();
+                    if (!isLevelCreated) CreateLevel(hit.pose.position);
+                }
+            }
+        }
+    }
+
+    private void CreateLevel(Vector3 positionSpawn)
+    {
+        Instantiate(LevelPrefab, positionSpawn, Quaternion.identity, levelWrapper.transform);
+        Catapult.SetActive(true);
+        isLevelCreated = true;
+    }
+}
