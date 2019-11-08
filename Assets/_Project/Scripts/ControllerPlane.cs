c5c582d7d6bd91d7a2a00200aa48a00f4dc6f1e6
Jason Liebault <jason.liebault@outlook.com>
2019-11-08 19:58:28 +0100

    Disabled planes

--- Assets/_Project/Scripts/ControllerPlane.cs
+++ Assets/_Project/Scripts/ControllerPlane.cs
@@ -1,10 +1,9 @@
-﻿using System.Collections;
+﻿using System;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.XR.ARFoundation;
 using UnityEngine.XR.ARSubsystems;
 using UnityEngine.UI;
-using System;
 
 public class ControllerPlane : MonoBehaviour
 {
@@ -17,87 +16,60 @@ public class ControllerPlane : MonoBehaviour
 
 
     public GameObject LevelPrefab;
-    public GameObject Catapult;
     public GameObject levelWrapper;
-    public GameObject JokerNullPrefab;
+    //public GameObject Catapult;
 
     private bool isLevelCreated = false;
-    private float _timeElapsed = 0;
-
 
     // Start is called before the first frame update
     void Start()
     {
         raycastManager = GetComponent<ARRaycastManager>();
         _planeManager = GetComponent<ARPlaneManager>();
-        Catapult.SetActive(false);
+        //Catapult.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
-        //ResetLog();
         if (Input.touchCount == 1)
         {
             List<ARRaycastHit> hitResults = new List<ARRaycastHit>();
+            
             raycastManager.Raycast(Input.GetTouch(0).position,hitResults, TrackableType.Planes);
-
-            if (hitResults.Count > 0)
+            
+            foreach (ARRaycastHit hit in hitResults)
             {
-                foreach (ARRaycastHit hit in hitResults)
+                if (!isLevelCreated)
                 {
-                    if (
-                        !isLevelCreated
-                        //&& hit.hitType == TrackableType.Planes
-                    )
-                    {
-                        Log(hit.pose.position.ToString());
-                        CreateLevel(hit.pose.position);
-                        DisablePlanes();
-                        break;
-                    }
+                    CreateLevel(hit.pose.position);
+                    DisablePlanes();
+                    break;
                 }
             }
         }
     }
 
-    private void ResetLog()
+    private void CreateLevel(Vector3 positionSpawn)
     {
-        _timeElapsed += Time.deltaTime;
-        if(_timeElapsed > 20)
+        isLevelCreated = true;
+        Log(positionSpawn.ToString());
+        Instantiate(LevelPrefab, positionSpawn, Quaternion.identity, levelWrapper.transform);
+        //Catapult.SetActive(true);
+    }
+
+    void DisablePlanes()
+    {
+        foreach (ARPlane plane in _planeManager.trackables)
         {
-            _timeElapsed = 0;
-            Logger.text = string.Empty;
+            plane.gameObject.SetActive(false);
         }
+        _planeManager.enabled = false;
     }
 
     private void Log(string value)
     {
         Debug.Log(value);
         Logger.text = Logger.text + "\n" + value;
-    }
-
-    private void CreateLevel(Vector3 positionSpawn)
-    {
-        Instantiate(LevelPrefab, positionSpawn, Quaternion.identity, levelWrapper.transform);
-        Catapult.SetActive(true);
-        isLevelCreated = true;
-        _planeManager.enabled = false;
-        Log("Plane Manager disabled");
-    }
-
-    void DisablePlanes()
-    {
-
-        foreach (ARPlane plane in FindObjectsOfType(Type.GetType("ARPlane")))
-        {
-            //plane.gameObject.SetActive(false);
-            plane.enabled = false;
-        }
-        Log("Planes should not be visibles");
-        _planeManager.planePrefab = JokerNullPrefab;
-        Log("JokerNullPrefab");
-
-        //_planeManager.enabled = false; 
     }
 }
