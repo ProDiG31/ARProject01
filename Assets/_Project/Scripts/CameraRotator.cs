c234db8a330540959b0886462f507630a208c745
Jason Liebault <jason.liebault@outlook.com>
2019-11-14 15:47:34 +0100

    Camera Rotation around the plane and add bunnies on start scene

---
+++ Assets/_Project/Scripts/CameraRotator.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraRotator : MonoBehaviour
+{
+    public GameObject Camera;
+    public float Speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Camera.transform.Rotate(0, Speed * Time.deltaTime, 0);
+    }
+}
