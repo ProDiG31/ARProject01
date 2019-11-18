fd95901ff9b31b5219306787101629486f5f558b
Romain <romain.ducros@cimpa.com>
2019-11-18 10:55:36 +0100

    Working at Rock trajectory

---
+++ Assets/FollowCam.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowCam : MonoBehaviour
+{
+    [Range(0,20)]
+    [SerializeField]
+    private float dist = 1;
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Camera.main.ScreenToWorldPoint(new Vector3(0.5f, 0.5f)) + (Camera.main.transform.forward * dist);
+    }
+}
