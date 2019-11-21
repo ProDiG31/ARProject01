931e5cc717156bcf04906d8caf031f6bdd2448cd
Jason Liebault <jason.liebault@outlook.com>
2019-11-21 13:00:44 +0100

    NavMesh + Warren + Multiple carrots

--- Assets/_Project/Scripts/MoveTo.cs
+++ Assets/_Project/Scripts/MoveTo.cs
@@ -5,19 +5,50 @@ using UnityEngine.AI;
 
 public class MoveTo : MonoBehaviour
 {
-    public Transform Destination;
+    public GameObject[] Carrots;
+    public Transform FinalDestination;
+
+    private NavMeshAgent _agent;
+    private bool finalStarted = false;
+    private int _choosenCarrotIndex = 0;
 
     // Start is called before the first frame update
     void Start()
     {
-        NavMeshAgent agent = GetComponent<NavMeshAgent>();
-        agent.destination = Destination.position;
+        _agent = GetComponent<NavMeshAgent>();
+        _agent.destination = Carrots[_choosenCarrotIndex].transform.position;
         
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(IsDestinationReached())
+        {
+            if (!finalStarted)
+            {
+                Carrots[_choosenCarrotIndex].SetActive(false);
+                _choosenCarrotIndex++;
+
+                if (_choosenCarrotIndex == Carrots.Length)
+                {
+                    Debug.Log("Go to warren");
+                    _agent.SetDestination(FinalDestination.position);
+                    finalStarted = true;
+                }
+                else
+                {
+                    _agent.SetDestination(Carrots[_choosenCarrotIndex].transform.position);
+                }
+            }
+            else
+            {
+                _agent.isStopped = true;
+            }
+        }
     }
+
+    private bool IsDestinationReached() =>
+        _agent.pathStatus == NavMeshPathStatus.PathComplete
+        && _agent.remainingDistance < 0.25f;
 }
