db3d01bf90c7cd0e6abc90394e2fe9b84c38f1ac
Romain <romain.ducros@cimpa.com>
2019-12-03 16:32:18 +0100

    Commit PC Cimpa

--- Assets/_Project/Scripts/Level/ExplosiveProps.cs
+++ Assets/_Project/Scripts/Level/ExplosiveProps.cs
@@ -2,34 +2,45 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class ExplosiveProps : DestructibleProps
 {
-    public float ExplosionRadius = .05f;
-    public float ExplosionForce = 10f; 
-    public float ExplosionDmg = 150f;
+    private float _ExplosionRadius = .5f;
+    private float _ExplosionForce = 10f; 
+    private static float _ExplosionDmg = 150f;
 
     public override void Die()
     {
         base.Die();
         GameObject sphere = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+        sphere.transform.position = transform.position;
+        SphereCollider collider = sphere.AddComponent<SphereCollider>();
+        StartCoroutine(ExplodeCoroutine(sphere, 1f));
+    }
 
-        sphere.transform.localScale = new Vector3(ExplosionRadius, ExplosionRadius, ExplosionRadius);
-
-        Collider[] colliders = Physics.OverlapSphere(transform.position, ExplosionRadius);
-
-        //foreach (Collider c in colliders)
-        //{
-        //    if (TryGetComponent(out DestructibleProps _destructibleProps))
-        //    {
-        //        float Distance = Vector3.Distance(transform.position, _destructibleProps.transform.position);
-        //        float DistRatio = ExplosionRadius / Distance;
-        //        _destructibleProps.ApplyDmg((int)(ExplosionDmg * DistRatio));
-        //        if(_destructibleProps._lifePoint > 0)
-        //        {
-        //            Vector3 direction = (transform.position - _destructibleProps.transform.position).normalized;
-        //            _destructibleProps.AddImpact(direction, ExplosionForce * DistRatio);
-        //        }
-        //    }
-        //}
+    private IEnumerator ExplodeCoroutine(GameObject sphere, float time)
+    {
+        Vector3 fromScale = Vector3.zero;
+        Vector3 toScale = new Vector3(_ExplosionRadius, _ExplosionRadius, _ExplosionRadius);
+        Material SphereMaterial = sphere.GetComponent<MeshRenderer>().material; 
+        float elapsedTime = 0;
+        while (elapsedTime < time)
+        {
+            float progress = (elapsedTime / time);
+            sphere.transform.localScale = Vector3.Lerp(fromScale, toScale, progress);
+            SphereMaterial.color = Color.Lerp(Color.red, Color.black, progress);
+            elapsedTime += Time.deltaTime;
+            yield return new WaitForEndOfFrame();
+        }
+        elapsedTime = 0;
+        while (elapsedTime < (time / 2))
+        {
+            float progress = (elapsedTime / (time / 2));
+            sphere.transform.localScale = Vector3.Lerp(toScale, fromScale,  progress);
+            SphereMaterial.color = Color.Lerp(Color.black, Color.clear, progress);
+            elapsedTime += Time.deltaTime;
+            yield return new WaitForEndOfFrame();
+        }
+        Destroy(sphere);
     }
 }
