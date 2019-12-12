4ba106af3199d45421d0bde862443036ce70d0a2
Romain <romain.ducros@cimpa.com>
2019-12-12 10:29:23 +0100

    Fixing LevelConteneurPrefab

--- Assets/LevelGenerator.cs
+++ Assets/LevelGenerator.cs
@@ -13,11 +13,19 @@ public class LevelGenerator : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        int levelId = PlayerPrefs.GetInt(ControllerMenu.LEVEL_INDEX_KEY, 0);
+        int _levelId = PlayerPrefs.GetInt(ControllerMenu.LEVEL_INDEX_KEY, 0);
+        string _gameMode = PlayerPrefs.GetString(ControllerMenu.GAME_MODE, " ");
 
-        if(levelId != 0)
+        if (_levelId != 0 && _gameMode != " ")
         {
-            _currentLevel = Instantiate(levelPrefabArcade[levelId], transform);
+            if (_gameMode == ControllerMenu.ARCADE_GAME_MODE)
+            {
+                _currentLevel = Instantiate(levelPrefabArcade[_levelId], transform);
+            }
+            else if (_gameMode == ControllerMenu.ARCADE_GAME_MODE)
+            {
+                _currentLevel = Instantiate(levelPrefabTime[_levelId], transform);
+            }
         }
     }
 
